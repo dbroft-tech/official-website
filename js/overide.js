9a0f4294962133ccd352d9009048a227dc5ebd45
D-Broft Technologies <dbroft.tech@proton.me>
2025-02-01 21:02:03 +0300

    commit

--- js/overide.js
+++ js/overide.js
@@ -1,54 +1,7 @@
-(() => {
-  'use strict'
-
-  // Theme Management
-  const storedTheme = localStorage.getItem('theme')
-  
-  const getPreferredTheme = () => {
-    if (storedTheme) {
-      return storedTheme
-    }
-    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
-  }
-
-  const setTheme = function (theme) {
-    if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      document.documentElement.setAttribute('data-bs-theme', 'dark')
-    } else {
-      document.documentElement.setAttribute('data-bs-theme', theme)
-    }
-  }
-
-  setTheme(getPreferredTheme())
-
-  const updateThemeIcon = (theme) => {
-    const themeIcon = document.querySelector('.theme-icon-active')
-    if (themeIcon) {
-      themeIcon.className = 'theme-icon-active bi me-2 ' + 
-        (theme === 'dark' ? 'bi-moon-stars-fill' : 
-         theme === 'light' ? 'bi-sun-fill' : 
-         'bi-circle-half')
-    }
-  }
-
-  const showActiveTheme = (theme) => {
-    const activeThemeIcon = document.querySelector('.theme-icon-active')
-    const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`)
-    
-    document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
-      element.classList.remove('active')
-    })
-    
-    btnToActive.classList.add('active')
-    updateThemeIcon(theme)
-  }
-
-  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
-    if (storedTheme !== 'light' && storedTheme !== 'dark') {
-      setTheme(getPreferredTheme())
-    }
-  })
-
+import { injectSpeedInsights } from '@vercel/speed-insights';
+injectSpeedInsights();
+import { inject } from "@vercel/analytics"
+inject();
   // Portfolio Filtering
   const initPortfolioFilters = () => {
     const filterButtons = document.querySelectorAll('[data-filter]')
@@ -155,4 +108,3 @@
     const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
     const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
   })
-})()
