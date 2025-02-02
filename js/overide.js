ad93b91fd58c4b18d1144adf4b4e7c0a68792e74
D-Broft Technologies <dbroft.tech@proton.me>
2025-02-02 11:23:07 +0300

    commit

--- js/overide.js
+++ js/overide.js
@@ -1,7 +1,5 @@
-import { injectSpeedInsights } from '@vercel/speed-insights';
-injectSpeedInsights();
-
-import { inject } from "@vercel/analytics"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
   // Portfolio Filtering
   const initPortfolioFilters = () => {
